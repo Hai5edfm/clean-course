6c9030b13076a0089a3e50512a373f9c5494b75b
hai5edfm <haisedfm@hai5e.com>
2024-10-02 05:49:46 -0600

    open closed principle

---
+++ src/solid/open-closed-a.ts
@@ -0,0 +1,20 @@
+import { PhotosService, PostService, TodoService } from './open-closed-b';
+import { HttpClient } from './open-closed-c';
+
+(async () => {
+
+    const httpClient = new HttpClient();
+
+    const todoService = new TodoService(httpClient);
+    const postService = new PostService(httpClient);
+    const photosService = new PhotosService(httpClient);
+
+    const todos = await todoService.getTodoItems();
+    const posts = await postService.getPosts();
+    const photos = await photosService.getPhotos();
+    
+    
+    console.log({ todos, posts, photos });
+    
+
+})();
---
+++ src/solid/open-closed-b.ts
@@ -0,0 +1,43 @@
+// import axios from 'axios';
+import { HttpClient } from './open-closed-c';
+
+
+export class TodoService { 
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+    async getTodoItems() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/todos/');
+        return data;
+    }
+}
+
+
+export class PostService {
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+    async getPosts() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
+        return data;
+    }
+}
+
+
+export class PhotosService {
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+    async getPhotos() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/photos');
+        return data;
+    }
+
+}
